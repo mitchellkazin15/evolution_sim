ff9a606742cb253bcabe1275c79d9b7627069137
mitchellkazin15 <39781454+mitchellkazin15@users.noreply.github.com>
2018-06-12 12:41:43 -0500

    Update neural_net.js

--- neural_net.js
+++ neural_net.js
@@ -1,3 +1,25 @@
-function sigmoid() {
+function sigmoidNeuron() {
+  this.inputs = new Array();
+  this.output = 0;
+  this.bias = 0;
+  this.fire = function() {
+    var sum = 0;
+    for(var i = 0; i < this.inputs.length; ++i){
+      sum += this.inputs[i].weight * this.inputs[i].neuron.output;
+    }
+    sum += this.bias;
+    this.output = 1 / (1 + Math.exp(-sum));
+  }
+}
 
+function connection(inputNeuron) {
+  this.weight = 0;
+  this.neuron = inputNeuron;
+}
+
+function network(numInputs,numOutputs,numHiddenLayers,hiddenSize){
+  this.layers = new Array(numHiddenLayers + 2);
+  for(var i = 0; i < this.layers.length; ++i){
+    
+  }
 }
